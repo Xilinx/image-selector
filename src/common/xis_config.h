43d190f92120d25252c85afbe087c2e5aaa017c1
Dasari Sharath Kumar <sharath.kumar.dasari@amd.com>
2025-03-19 10:59:28 +0530

    Updated Bank A/B offsets for ZynqMP platforms
    Signed-off-by: Dasari Sharath Kumar <sharath.kumar.dasari@amd.com>

--- src/common/xis_config.h
+++ src/common/xis_config.h
@@ -43,8 +43,8 @@ extern "C" {
 /**************************** Type Definitions *******************************/
 
 /***************** Macros (Inline Functions) Definitions *********************/
-#define XIS_XLNX_LENGTH                0x4U
-#define XIS_SIZE_1KB                0x400U
+#define XIS_XLNX_LENGTH              0x4U
+#define XIS_SIZE_1KB                 0x400U
 #define XIS_SIZE_32KB                0x8000U
 
 /*
@@ -62,35 +62,35 @@ extern "C" {
  */
 
 #ifdef XIS_VERSAL_PLAT
-	#define XIS_UPDATE_A_B_MECHANISM
-	#define XIS_FWU_UPDATE
-	//#define XIS_QSPI_FLSH
-	#define XIS_OSPI_FLSH
-    #define XIS_MDATA_OFFSET            0x1500000U
-    #define XIS_MDATA_BKP_OFFSET        0x1520000U
-    #define XIS_BANK_0_OFFSET           0x1580000U
-    #define XIS_BANK_1_OFFSET           0x87C0000U
-    #define XIS_RECOVERY_OFFSET         0xE0000U
+#define XIS_UPDATE_A_B_MECHANISM
+#define XIS_FWU_UPDATE
+//#define XIS_QSPI_FLSH
+#define XIS_OSPI_FLSH
+#define XIS_MDATA_OFFSET             0x1500000U
+#define XIS_MDATA_BKP_OFFSET         0x1520000U
+#define XIS_BANK_0_OFFSET            0x1580000U
+#define XIS_BANK_1_OFFSET            0x87C0000U
+#define XIS_RECOVERY_OFFSET          0xE0000U
 #else
-	//#define XIS_UPDATE_A_B_MECHANISM
-	#define XIS_GET_BOARD_PARAMS
-	//#define XIS_FWU_UPDATE
-	//#define XIS_QSPI_FLSH
-    //#define XIS_UART_ENABLE
-    #define XIS_MDATA_OFFSET            0x100000U
-    #define XIS_MDATA_BKP_OFFSET        0x120000U
-    #define XIS_BANK_0_OFFSET           0x200000U
-    #define XIS_BANK_1_OFFSET           0xF80000U
-    #define XIS_RECOVERY_OFFSET         0x1E00000U
+#define XIS_UPDATE_A_B_MECHANISM
+//#define XIS_GET_BOARD_PARAMS
+#define XIS_FWU_UPDATE
+#define XIS_QSPI_FLSH
+#define XIS_UART_ENABLE
+#define XIS_MDATA_OFFSET             0x1500000U
+#define XIS_MDATA_BKP_OFFSET         0x1520000U
+#define XIS_BANK_0_OFFSET            0x1580000U
+#define XIS_BANK_1_OFFSET            0x25C0000U
+#define XIS_RECOVERY_OFFSET          0xE0000U
 #endif
 
 #ifdef XPAR_XIICPS_NUM_INSTANCES
-    #ifndef XIS_UPDATE_A_B_MECHANISM
-        #define XIS_GET_BOARD_PARAMS
-    #endif
-    #define XIS_MUX_ADDR             (0x74U)
-    #define XIS_I2C_MUX_INDEX        (0x1U)
-    #define XIS_EEPROM_ADDRESS       (0x54U)
+#ifndef XIS_UPDATE_A_B_MECHANISM
+#define XIS_GET_BOARD_PARAMS
+#endif
+#define XIS_MUX_ADDR                 0x74U
+#define XIS_I2C_MUX_INDEX            0x1U
+#define XIS_EEPROM_ADDRESS           0x54U
 #endif
 
 #ifdef __cplusplus
